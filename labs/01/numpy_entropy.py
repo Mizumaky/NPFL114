f966b2db507d42da7923862fa3e8216a45e6bd3f
Mizumaky <m.muller007@seznam.cz>
2022-02-23 21:50:52 +0100

    uloha numpy entropy hotova

--- labs/01/numpy_entropy.py
+++ labs/01/numpy_entropy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-
+from typing import List, Dict
 import numpy as np
 
 parser = argparse.ArgumentParser()
@@ -11,39 +11,67 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Evalu
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args: argparse.Namespace) -> tuple[float, float, float]:
-    # TODO: Load data distribution, each line containing a datapoint -- a string.
+    data_dist: Dict[str, int] = {}  # mapping of symbols to their count
+    n: int = 0  # size of input data
+    # Load data distribution
     with open(args.data_path, "r") as data:
         for line in data:
             line = line.rstrip("\n")
-            # TODO: Process the line, aggregating data with built-in Python
-            # data structures (not NumPy, which is not suitable for incremental
-            # addition and string mapping).
+            if line in data_dist:
+                data_dist[line] += 1
+            else:
+                data_dist[line] = 1
+            n += 1
+    k: int = len(data_dist)  # number of different input symbols
+    # NumPy array containing the data distribution.
+    np_data = np.empty(k, dtype=float)  # np array for symbol probabilities (unordered)
+    # fill np array with data
+    for i, val in enumerate(data_dist.values()):
+        np_data[i] = val/n
 
-    # TODO: Create a NumPy array containing the data distribution. The
-    # NumPy array should contain only data, not any mapping. Alternatively,
-    # the NumPy array might be created after loading the model distribution.
-
-    # TODO: Load model distribution, each line `string \t probability`.
+    # Load model distribution, each line `string \t probability`.
+    model_dist: Dict[str, float] = {}
     with open(args.model_path, "r") as model:
         for line in model:
             line = line.rstrip("\n")
-            # TODO: process the line, aggregating using Python data structures
+            key, val = line.split()
+            model_dist[key] = float(val)
 
-    # TODO: Create a NumPy array containing the model distribution.
+    # NumPy array containing the model distribution
+    # np_model = np.fromiter(model_dist.values(), dtype=float)  # np array for symbol probabilities (unordered - PROBLEM)
+    np_model = np.empty(k, dtype=float)  # np array for symbol probabilities (ordered to correspond with np_data)
+    missing_symbol = False
+    for i, key in enumerate(data_dist.keys()):
+        if key in model_dist:
+            np_model[i] = model_dist[key]
+        else:
+            # we have a data symbol, which is not in model symbols, so we cannot calculate crossentropy
+            missing_symbol = True
+            break
 
-    # TODO: Compute the entropy H(data distribution). You should not use
+    # The reason np.array(Samples.values()) doesn't give what you expect in Python 3 is that in Python 3,
+    # the values() method of a dict returns an iterable view, whereas in Python 2,
+    # it returns an actual list of the keys. (so one cannot np.array(list(Samples.values())))
+
+    # Compute the entropy H(data distribution). You should not use
     # manual for/while cycles, but instead use the fact that most NumPy methods
     # operate on all elements (for example `*` is vector element-wise multiplication).
-    entropy = None
+    entropy = -(np_data * np.log(np_data)).sum()  # bonus - computational time reference https://stackoverflow.com/questions/15450192/fastest-way-to-compute-entropy-in-python
 
-    # TODO: Compute cross-entropy H(data distribution, model distribution).
+    # print(np_data)
+    # print(np_model)
+
+    # Compute cross-entropy H(data distribution, model distribution).
     # When some data distribution elements are missing in the model distribution,
     # return `np.inf`.
-    crossentropy = None
+    if not missing_symbol:
+        crossentropy = -(np_data * np.log(np_model)).sum()
+    else:
+        crossentropy = np.inf
 
-    # TODO: Compute KL-divergence D_KL(data distribution, model_distribution),
+    # Compute KL-divergence D_KL(data distribution, model_distribution),
     # again using `np.inf` when needed.
-    kl_divergence = None
+    kl_divergence = crossentropy - entropy
 
     # Return the computed values for ReCodEx to validate
     return entropy, crossentropy, kl_divergence
